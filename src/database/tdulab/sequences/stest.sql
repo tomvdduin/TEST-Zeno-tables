8782e06bddc0f987679f8a081f7cfcaefb882bef
Tom van der Duin <tduin@socho.nl>
2026-07-06 14:30:50 +0200

    table TEST

---
+++ src/database/tdulab/sequences/stest.sql
@@ -0,0 +1,5 @@
+create sequence tdulab.stest minvalue 1 maxvalue 9999999999999999999999999999 increment by 1 /* start with n */ nocache noorder nocycle
+nokeep noscale global;
+
+
+-- sqlcl_snapshot {"hash":"e338397b85ed8e98c4d8f95b5bfc6bb2767e82b0","type":"SEQUENCE","name":"STEST","schemaName":"TDULAB","sxml":"\n  <SEQUENCE xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>TDULAB</SCHEMA>\n   <NAME>STEST</NAME>\n   \n   <INCREMENT>1</INCREMENT>\n   <MINVALUE>1</MINVALUE>\n   <MAXVALUE>9999999999999999999999999999</MAXVALUE>\n   <CACHE>0</CACHE>\n   <SCALE>NOSCALE</SCALE>\n</SEQUENCE>"}
---
+++ src/database/tdulab/tables/test.sql
@@ -0,0 +1,8 @@
+create table tdulab.test (
+    tst_pk number,
+    tst_cd varchar2(10 byte),
+    tst_nm varchar2(120 byte)
+);
+
+
+-- sqlcl_snapshot {"hash":"0b64fdf76f8ed071bdb6837840bb25d725ea5373","type":"TABLE","name":"TEST","schemaName":"TDULAB","sxml":"\n  <TABLE xmlns=\"http://xmlns.oracle.com/ku\" version=\"1.0\">\n   <SCHEMA>TDULAB</SCHEMA>\n   <NAME>TEST</NAME>\n   <RELATIONAL_TABLE>\n      <COL_LIST>\n         <COL_LIST_ITEM>\n            <NAME>TST_PK</NAME>\n            <DATATYPE>NUMBER</DATATYPE>\n         </COL_LIST_ITEM>\n         <COL_LIST_ITEM>\n            <NAME>TST_CD</NAME>\n            <DATATYPE>VARCHAR2</DATATYPE>\n            <LENGTH>10</LENGTH>\n            <COLLATE_NAME>USING_NLS_COMP</COLLATE_NAME>\n         </COL_LIST_ITEM>\n         <COL_LIST_ITEM>\n            <NAME>TST_NM</NAME>\n            <DATATYPE>VARCHAR2</DATATYPE>\n            <LENGTH>120</LENGTH>\n            <COLLATE_NAME>USING_NLS_COMP</COLLATE_NAME>\n         </COL_LIST_ITEM>\n      </COL_LIST>\n      <DEFAULT_COLLATION>USING_NLS_COMP</DEFAULT_COLLATION>\n      <PHYSICAL_PROPERTIES>\n         <HEAP_TABLE></HEAP_TABLE>\n      </PHYSICAL_PROPERTIES>\n   </RELATIONAL_TABLE>\n</TABLE>"}
